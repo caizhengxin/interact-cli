8a226f395e3de85151fbf6869e0afe68da37ea44
JanKinCai <caizhengxin16@163.com>
2019-12-12 23:18:11 +0800

    test(tests): add tests

---
+++ tests/test_loads.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: JanKinCai
+# @Date:   2019-12-12 22:49:39
+# @Last Modified by:   JanKinCai
+# @Last Modified time: 2019-12-12 22:54:55
+import os
+
+from interact import loads
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+json_file = os.path.join(base_dir, "interact.json")
+
+
+if __name__ == "__main__":
+    print(loads(json_file))
