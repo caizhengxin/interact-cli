73e074850002befee7ee884bb425a0a54d0fa6b1
JanKinCai <caizhengxin16@163.com>
2019-12-24 22:53:28 +0800

    feat(demo): update demo

--- demo/interact_demo3.py
+++ demo/interact_demo3.py
@@ -2,10 +2,11 @@
 # @Author: JanKinCai
 # @Date:   2019-12-24 22:32:12
 # @Last Modified by:   JanKinCai
-# @Last Modified time: 2019-12-24 22:32:12
+# @Last Modified time: 2019-12-24 22:43:14
 from interact import interacts
 
 
 if __name__ == "__main__":
     obj = interacts("interact.json")
     print(obj)
+    print(obj.version)
