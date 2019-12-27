43b4b7a21e12fac6e1b0f8125cecb3d36cdca5cf
JanKinCai <caizhengxin16@163.com>
2019-12-27 21:39:24 +0800

    refactor(demo): update regex demo

--- demo/interact_regex.py
+++ demo/interact_regex.py
@@ -2,8 +2,16 @@
 # @Author: JanKinCai
 # @Date:   2019-12-26 23:15:03
 # @Last Modified by:   JanKinCai
-# @Last Modified time: 2019-12-26 23:20:48
+# @Last Modified time: 2019-12-27 21:37:07
 from interact import load
+
+
+"""
+IPv4 address [192.168.166.12]: 22
+Error: Invalided `22`
+IPv4 address [192.168.166.12]: 192.168.166.2
+{'ipv4': '192.168.166.2'}
+"""
 
 
 if __name__ == "__main__":
