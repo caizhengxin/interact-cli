195cc7bc6226997cf1c12a3eabdbace423d4a38d
JanKinCai <caizhengxin16@163.com>
2020-01-06 22:21:06 +0800

    feat(when.py): add rich when keywords

--- interact/utils.py
+++ interact/utils.py
@@ -2,22 +2,47 @@
 # @Author: JanKinCai
 # @Date:   2019-12-24 10:46:50
 # @Last Modified by:   JanKinCai
-# @Last Modified time: 2019-12-24 10:47:08
+# @Last Modified time: 2020-01-06 22:16:54
 
 
 class CmdInputDict(dict):
-    """
-    CmdInputDict
+    """CmdInputDict
     """
 
-    def __getattr__(self, name: str):
-        """
-        getattr
+    def __init__(self, interable: dict, *args, **kwargs):
+        """init
         """
 
-        v = self[name]
+        interable.update(self.do_interable(interable))
+        super().__init__(interable)
+
+    def do_interable(self, interable: dict) -> dict:
+        """Deal args
+        """
+
+        items = {}
+
+        for k, v in interable.items():
+            if isinstance(v, dict):
+                items.update(self.do_interable(v))
+            else:
+                items[v] = v
+
+        return items
+
+    def __getitem__(self, name: str) -> any:
+        """getitem
+        """
+
+        v = self.get(name)
 
         if isinstance(v, dict):
-            return CmdInputDict(v)
+            return self.__class__(v)
 
         return v
+
+    def __getattr__(self, name: str) -> any:
+        """getattr
+        """
+
+        return self.__getitem__(name)
--- interact/when.py
+++ interact/when.py
@@ -2,17 +2,12 @@
 # @Author: JanKinCai
 # @Date:   2019-12-23 22:45:03
 # @Last Modified by:   JanKinCai
-# @Last Modified time: 2020-01-03 16:57:00
-import re
-
-from typing import (
-    Optional, Tuple, Union, Any
-)
+# @Last Modified time: 2020-01-06 22:17:44
+# from typing import (
+#     Optional, Tuple, Union, Any
+# )
 
 from interact.utils import CmdInputDict
-
-
-reg = re.compile(r"^([a-zA-Z_][0-9a-zA-Z_.]*) (==|!=) ([\S\s]+)$")
 
 
 class When(object):
@@ -23,119 +18,32 @@ class When(object):
     :param cid(dict): Cmd input data.
     """
 
-    mapping_boolean_true = [
-        "y", "Y",
-        "t", "T",
-        "true", "True",
-        True,
-    ]
-
-    mapping_boolean_false = [
-        "n", "N",
-        "f", "F",
-        "false", "False",
-        False,
-    ]
-
     def __init__(self, wv: str, cid: dict, *args, **kwargs) -> None:
         """
         init
         """
 
-        self.wv = wv
+        self.wv = self.do_when_value(wv)
         self.cid = CmdInputDict(cid)
-        self.attr = ""
 
-    def get_cid_value(self, name: str) -> Union[str, int, float, bool, dict]:
-        """
-        Get cmd input data.
+    def do_when_value(self, wv: str) -> str:
+        """Deal when value.
 
-        :param name(str): Cmd input data(key)
+        Args:
+            param wv (str): When value.
 
-        :return: Union[str, int, float, bool, dict]
-        """
-
-        nlist = name.split(".")
-        cid = self.cid
-
-        for n in nlist:
-            cid = getattr(cid, n, None)
-
-            if not isinstance(cid, CmdInputDict):
-                break
-
-        return cid
-
-    def split_when(self) -> Optional[Tuple[str, str, str]]:
-        """
-        Split when value.
-
-        :return: Optional[Tuple[str, str, str]]
+        Returns:
+            str: When value.
         """
 
-        v = reg.match(self.wv)
-
-        if v is not None:
-            return v.group(1), v.group(2), v.group(3)
-
-        return v
+        return wv.replace("false", "False").replace("true", "True")
 
     def do_when(self) -> bool:
         """
         Deal with when.
         """
 
-        wvt = self.split_when()
-
-        if wvt is None:
-            return False
-
-        self.attr, operator, value = wvt
-
-        if operator == "==":
-            return self == value
-        elif operator == "!=":
-            return self != value
-
-        return False
-
-    def get_other(self, other: Any) -> Tuple[Any, Any]:
-        """
-        To when value
-        """
-
-        v: Any = None
-
-        cid = self.get_cid_value(self.attr or "")
-
-        if other in self.mapping_boolean_false:
-            v = False
-        elif other in self.mapping_boolean_true:
-            v = True
-        elif isinstance(cid, str):
-            v = other.lstrip("'").rstrip("'").lstrip('"').rstrip('"')
-        else:
-            v = type(cid)(other)
-
-        return cid, v
-
-    def __eq__(self, other):
-        """
-        Operator ==
-        """
-
-        cid, other = self.get_other(other)
-
-        return cid == other
-
-    def __ne__(self, other):
-        """
-        Operator !=
-        """
-
-        cid, other = self.get_other(other)
-
-        return cid != type(cid)(other)
+        return eval(self.wv, self.cid)
 
 
 def when(*args, **kwarags) -> bool:
@@ -143,7 +51,4 @@ def when(*args, **kwarags) -> bool:
     Associate rules
     """
 
-    try:
-        return When(*args, **kwarags).do_when()
-    except KeyError:
-        raise ValueError("Key(when) value is wrong.")
+    return When(*args, **kwarags).do_when()
